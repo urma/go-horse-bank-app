295b8ecafb46c4ebf6a2084590ed969ac9bf0e2e
Ulisses Albuquerque <ulisses.albuquerque@team.telstra.com>
2017-11-14 10:30:39 +1100

    Login controller and views

---
+++ app/controllers/login.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  req.app.locals.logger.info('Login form');
+  res.render(path.join('login', 'index.pug'));
+});
+
+router.post('/', (req, res, next) => {
+  req.app.locals.logger.info('Request parameters', req.body);
+  res.render(path.join('login', 'index.pug'));
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,35 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const winston = require('winston');
+
+const app = express();
+
+app.locals.logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({ colorize: true, timestamp: true }),
+  ],
+});
+
+/* Enable request body parsing */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Configure paths */
+app.set('views', path.join(__dirname, 'app', 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'app', 'public')));
+
+/* Log requests */
+app.use((req, res, next) => {
+  app.locals.logger.info(req.ip, req.method, req.url);
+  next();
+});
+
+/* Mount controller paths */
+app.use('/login', require(path.join(__dirname, 'app', 'controllers', 'login')));
+
+/* Start server */
+app.listen(3000, () => {
+  app.locals.logger.info('Web server started at http://127.0.0.1:3000/');
+});

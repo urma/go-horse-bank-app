02083f58e9c3da8cb7248ae21f089782ed82ea54
Ulisses Albuquerque <ulisses.albuquerque@team.telstra.com>
2017-11-14 12:13:15 +1100

    Fix git-secrets invocation in pre-commit

--- precommit.sh
+++ precommit.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-basedir=$(dirname $0)
-
 # Chain all tests and fail fast in case any of them has issues
-git-secrets --scan ${basedir} && \
-  ${basedir}/node_modules/.bin/eslint ${basedir}
+git-secrets --scan && \
+  ./node_modules/.bin/eslint .
